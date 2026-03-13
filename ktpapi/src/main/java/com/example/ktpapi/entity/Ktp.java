e1fc243a40b42051a02da7e28eba2d4313319dda
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:41:49 +0700

    menambahkan kode ktp

--- ktpapi/src/main/java/com/example/ktpapi/entity/Ktp.java
+++ ktpapi/src/main/java/com/example/ktpapi/entity/Ktp.java
@@ -1,4 +1,31 @@
 package com.example.ktpapi.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ktp")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String nomorKtp;
+
+    private String namaLengkap;
+
+    private String alamat;
+
+    private LocalDate tanggalLahir;
+
+    private String jenisKelamin;
+
 }
