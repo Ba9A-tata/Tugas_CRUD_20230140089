e4d73bb79255840031c29d1acad840239951a5e2
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:46:11 +0700

    menambahkan kode KtpController

--- ktpapi/src/main/java/com/example/ktpapi/controller/KtpController.java
+++ ktpapi/src/main/java/com/example/ktpapi/controller/KtpController.java
@@ -1,4 +1,46 @@
 package com.example.ktpapi.controller;
 
+import com.example.ktpapi.dto.KtpDTO;
+import com.example.ktpapi.entity.Ktp;
+import com.example.ktpapi.service.KtpService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ktp")
+@CrossOrigin
 public class KtpController {
+
+    private final KtpService service;
+
+    public KtpController(KtpService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public Ktp create(@RequestBody KtpDTO dto){
+        return service.create(dto);
+    }
+
+    @GetMapping
+    public List<Ktp> getAll(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Ktp getById(@PathVariable Integer id){
+        return service.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Ktp update(@PathVariable Integer id, @RequestBody KtpDTO dto){
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        service.delete(id);
+    }
 }
