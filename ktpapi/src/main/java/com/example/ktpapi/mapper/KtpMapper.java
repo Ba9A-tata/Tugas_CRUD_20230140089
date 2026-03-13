5a69e25bd09b49f612e09eb82c5e5c90b88bf8d5
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:43:12 +0700

    menambahkan kode KtpMapper

--- ktpapi/src/main/java/com/example/ktpapi/mapper/KtpMapper.java
+++ ktpapi/src/main/java/com/example/ktpapi/mapper/KtpMapper.java
@@ -1,4 +1,21 @@
 package com.example.ktpapi.mapper;
 
+import com.example.ktpapi.dto.KtpDTO;
+import com.example.ktpapi.entity.Ktp;
+
 public class KtpMapper {
+
+    public static Ktp toEntity(KtpDTO dto){
+
+        Ktp ktp = new Ktp();
+
+        ktp.setNomorKtp(dto.getNomorKtp());
+        ktp.setNamaLengkap(dto.getNamaLengkap());
+        ktp.setAlamat(dto.getAlamat());
+        ktp.setTanggalLahir(dto.getTanggalLahir());
+        ktp.setJenisKelamin(dto.getJenisKelamin());
+
+        return ktp;
+    }
+
 }
