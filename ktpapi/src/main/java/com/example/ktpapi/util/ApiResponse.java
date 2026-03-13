b30a37ea8d102e9d744963958f8d21e7c5904060
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:46:54 +0700

    menambahkan kode ApiResponse

--- ktpapi/src/main/java/com/example/ktpapi/util/ApiResponse.java
+++ ktpapi/src/main/java/com/example/ktpapi/util/ApiResponse.java
@@ -1,4 +1,13 @@
 package com.example.ktpapi.util;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private String message;
+    private T data;
+
 }
