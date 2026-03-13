7c2733091cc45d7100f1498c9c08285af867619e
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:42:50 +0700

    menambahkan kode KtpRepository

--- ktpapi/src/main/java/com/example/ktpapi/repository/KtpRepository.java
+++ ktpapi/src/main/java/com/example/ktpapi/repository/KtpRepository.java
@@ -1,4 +1,12 @@
 package com.example.ktpapi.repository;
 
-public class KtpRepository {
+import com.example.ktpapi.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+
 }
