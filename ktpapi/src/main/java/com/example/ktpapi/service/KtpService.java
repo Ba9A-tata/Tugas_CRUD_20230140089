234bf7d7d08768503e45d57c2bb46c3cc4520f32
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:45:42 +0700

    menambahkan kode KtpService

--- ktpapi/src/main/java/com/example/ktpapi/service/KtpService.java
+++ ktpapi/src/main/java/com/example/ktpapi/service/KtpService.java
@@ -1,4 +1,20 @@
 package com.example.ktpapi.service;
 
-public class KtpService {
+import com.example.ktpapi.dto.KtpDTO;
+import com.example.ktpapi.entity.Ktp;
+
+import java.util.List;
+
+public interface KtpService {
+
+    Ktp create(KtpDTO dto);
+
+    List<Ktp> getAll();
+
+    Ktp getById(Integer id);
+
+    Ktp update(Integer id, KtpDTO dto);
+
+    void delete(Integer id);
+
 }
