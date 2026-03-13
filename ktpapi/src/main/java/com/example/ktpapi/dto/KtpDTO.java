3243bf7ea297506db252b1443721d28a9f67ec54
Ba9A-tata <yogadzakkii@gmail.com>
2026-03-13 21:42:15 +0700

    menambahkan kode ktpdto

--- ktpapi/src/main/java/com/example/ktpapi/dto/KtpDTO.java
+++ ktpapi/src/main/java/com/example/ktpapi/dto/KtpDTO.java
@@ -1,4 +1,15 @@
 package com.example.ktpapi.dto;
 
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class KtpDTO {
+
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+
 }
